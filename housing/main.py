a5986789b445fbb60d72b75ad64e2b2200050906
Yang <yangsu.personal@gmail.com>
2019-07-16 23:14:12 -0500

    adding some comments and add visualization for the geographical data

--- housing/main.py
+++ housing/main.py
@@ -19,10 +19,23 @@ def main():
     print_full(housing_data.describe())  # prints the statistical summary of the housing_data
     housing_data.hist(bins=50, figsize=(20, 15))
 
+    # split the dataset between training sets and test sets.
+    # use stratified approach when dealing with small sets of data since we could bias the sampling
+    # ( we want the sets of data to represent the overall population)
     training_set, test_set = stratified_split(housing_data)
     training_set.hist(bins=50, figsize=(20, 15))
     test_set.hist(bins=50, figsize=(20, 15))
     plt.show()
+
+    # visualizes data to try to see patterns
+    visualize_housing_data(housing_data)
+    plt.show()
+
+
+def visualize_housing_data(data):
+    cp = data.copy()
+    cp.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4, s=cp["population"] / 100, label="population",
+            c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True, figsize=(10, 7))
 
 
 def stratified_split(data, test_size=0.2, seed=42):
